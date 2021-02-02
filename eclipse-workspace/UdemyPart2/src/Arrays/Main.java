ce51767bec8709d3b2d80240ada42c7fcfe404a1
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-02-02 21:38:58 +0200

    MainArray1

---
+++ eclipse-workspace/UdemyPart2/src/Arrays/Main.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int [] myVariable = new int[10];
+		for(int i =0; i<10; i++) {
+			myVariable[i]=i*10;
+		}
+		for (int i=0; i<10;i++) {
+			System.out.println("Element " + i + " value is " + myVariable[i]);
+		}
+		
+//		myVariable [5]=50;// actually 6 
+//
+//		double [] myDoubleArray = new double[10];
+//		System.out.println(myVariable[5]); 
+		}
+
+}
--- eclipse-workspace/UdemyPart2/src/Main.java
+++ eclipse-workspace/UdemyPart2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 	
 		ITelephone timPhone;
-		timPhone = new DeskPhone (5455);
+		timPhone = new DeskPhone (5455656, false);
 		
 		timPhone.answer();
 		timPhone.callPhone(45345345);
